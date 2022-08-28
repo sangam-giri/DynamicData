0196f72d36bcb22ac3fdec84ded30ae23687b53d
Sangam Giri <girisangam00@gmail.com>
2022-08-28 18:40:19 +0545

    variables

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:test/variables.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Name: $name'),
+            Text('Age: $age'),
+            Text('Maritial Status: $status')
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/variables.dart
@@ -0,0 +1,5 @@
+//Keep all the global variables here
+
+var name = "King Kong";
+var age = 500;
+var status = true;
